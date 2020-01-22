39a9c5c4f9ba53fdce75d5b9e90227f12c135bd3
Andy Turner <A.G.D.Turner@leeds.ac.uk>
2020-01-22 07:29:12 +0000

    Package name update.

--- src/main/java/uk/ac/leeds/ccg/stats/Generic_Statistics.java
+++ src/main/java/uk/ac/leeds/ccg/stats/Generic_Statistics.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package uk.ac.leeds.ccg.andyt.stats;
+package uk.ac.leeds.ccg.stats;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
